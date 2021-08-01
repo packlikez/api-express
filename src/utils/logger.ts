bc1e9e9671bf19b753deb8c20b3df7782873761d
packkrittaphat <krittaphatkmitl@gmail.com>
2021-08-01 20:16:01 +0700

    Fix logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,8 +5,10 @@ const { format, transports } = winston;
 const logFormat = format.combine(format.colorize(), format.simple());
 
 const logger = winston.createLogger({
-  level: "http",
+  level: "debug",
   transports: [new transports.Console({ format: logFormat })],
 });
+
+if (process.env.NODE_ENV === "production") logger.level = "http";
 
 export default logger;
