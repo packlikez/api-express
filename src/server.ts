5751096a12edfe96e733397c19e0d5d7f512e21b
packkrittaphat <krittaphatkmitl@gmail.com>
2021-08-01 19:01:15 +0700

    Add prettier in eslint and format code

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express from 'express'
-import logger from "./utils/logger";
+import express from "express";
 
-const app = express()
+import logger from "./utils/logger";
+import routes from "./routes";
+
+const app = express();
 
 const PORT = process.env.PORT||4000
 app.listen(PORT,()=>logger.info(`Listening on port: ${PORT}`))
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,17 +1,12 @@
 import winston from "winston";
 
-const {format,transports} = winston
+const { format, transports } = winston;
 
-const logFormat = format.combine(
-    format.colorize(),
-    format.simple()
-)
+const logFormat = format.combine(format.colorize(), format.simple());
 
 const logger = winston.createLogger({
-    level:'http',
-    transports:[
-        new transports.Console({ format:logFormat })
-    ]
-})
+  level: "http",
+  transports: [new transports.Console({ format: logFormat })],
+});
 
-export default logger
+export default logger;
